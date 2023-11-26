fc7b21ef214e23dbeaea53eae918acdaa879a7e1
john-minney-iii <jminneyiii@gmail.com>
2023-11-26 13:58:20 -0700

    Added Auth0 provider

--- src/util/next-auth-options.ts
+++ src/util/next-auth-options.ts
@@ -2,12 +2,18 @@ import type { AuthOptions } from "next-auth";
 
 // Providers
 import GoogleProvider from "next-auth/providers/google";
+import Auth0Provider from "next-auth/providers/auth0";
 
 const authOptions: AuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.NEXTAUTH_GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.NEXTAUTH_GOOGLE_SECRET || "",
+    }),
+    Auth0Provider({
+      clientId: process.env.NEXTAUTH_AUTH0_CLIENT_ID || "",
+      clientSecret: process.env.NEXTAUTH_AUTH0_SECRET || "",
+      issuer: process.env.NEXTAUTH_AUTH0_ISSUER,
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET || "",
